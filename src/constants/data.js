078b08688adc076df5c69a106c7008b7a97a316a
giadz2005 <quocgia2005@gmail.com>
2025-12-25 13:05:22 +0700

    Initial commit: Config

---
+++ src/constants/data.js
@@ -0,0 +1,25 @@
+export const COFFEE_LIST = [
+    {
+        id: '1',
+        name: 'Americano',
+        price: 3.00,
+        image: 'https://bit.ly/coffee-americano', // Sau này thay bằng ảnh thật
+        description: 'A rich and smooth classic coffee.',
+    },
+    {
+        id: '2',
+        name: 'Cappuccino',
+        price: 3.50,
+        image: 'https://bit.ly/coffee-cappuccino',
+        description: 'Steamed milk foam with a double shot of espresso.',
+    },
+    // Thêm các món khác ở đây...
+];
+
+export const COLORS = {
+    primary: '#006241', // Màu xanh đặc trưng của quán cafe
+    secondary: '#D4A574',
+    background: '#FFFFFF',
+    text: '#333333',
+    grey: '#F5F5F5'
+};
